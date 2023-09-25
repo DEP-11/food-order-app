6e5261587c311e0b39aecf2208b90590a01fa7bb
Nilan Dinushka <nilandinushka5@gmail.com>
2023-09-25 19:33:26 +0530

    Create controls

--- kitchen-app/client-app/src/main/java/lk/ijse/dep11/ClintSceneController.java
+++ kitchen-app/client-app/src/main/java/lk/ijse/dep11/ClintSceneController.java
@@ -1,4 +1,26 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ClintSceneController {
+    public Button btnNewCustomer;
+    public Button btnPlaceOrder;
+    public TextField txtID;
+    public TextField txtName;
+    public TextField txtContactNumber;
+    public Spinner spnBurger;
+    public Spinner spnSub;
+    public Spinner spnPepsi;
+    public Spinner spnCoke;
+    public TableView tblOrderDetails;
+
+    public void btnNewCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlaceOrderOnAction(ActionEvent actionEvent) {
+    }
 }
