25d772253636d1ff902a2462392eec6925819e93
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-09-25 19:40:55 +0530

    Add shared app

---
+++ kitchen-app/shared-app/src/main/java/OrderDetails.java
@@ -0,0 +1,13 @@
+public class OrderDetails {
+    int burger;
+    int submarine;
+    int coke;
+    int pepsi;
+
+    public OrderDetails(int burger, int submarine, int coke, int pepsi) {
+        this.burger = burger;
+        this.submarine = submarine;
+        this.coke = coke;
+        this.pepsi = pepsi;
+    }
+}
